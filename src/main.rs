aaa6236fe8a778dd61cad9a0efaff47cef1e4628
thisisencrypt0r <wahyu403.py@gmail.com>
2021-05-30 20:43:27 +0700

    alpha commit

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@ use rand::Rng;
 use std::fs::File;
 use std::io::prelude::*;
 
+struct Letter {
+    character: char,
+    revealed: bool
+}
+
 fn main() {
     let keyword = keyword();
+    let letters = create_letters(&keyword);
 
-    println!("The Generated Word is {}", keyword);
+    display_progress(&letters);
+    println!("[DEBUG] The selected keyword above is {}!", keyword);
 }
 
 fn keyword() -> String {
@@ -21,4 +28,35 @@ fn keyword() -> String {
     let random_index = rand::thread_rng().gen_range(0, available_word.len());
 
     return String::from(available_word[random_index]);
+}
+
+fn create_letters(word: &String) -> Vec<Letter> {
+    let mut letters: Vec<Letter> = Vec::new();
+
+    for c in word.chars(){
+        letters.push(Letter{
+            character: c,
+            revealed: false
+        });
+    }
+
+    return letters;
+}
+
+fn display_progress(letters: &Vec<Letter>){
+    let mut display_string = String::from("Progress: ");
+
+    for letter in letters{
+        display_string.push(' ');
+
+        if letter.revealed{
+            display_string.push(letter.character);
+        } else{
+            display_string.push('_');
+        }
+
+        display_string.push(' ');
+    }
+
+    println!("{}", display_string);
 }
